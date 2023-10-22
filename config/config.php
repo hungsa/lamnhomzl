0656020691391e31142361687659411ede91905d
hungsa <105655347+hungsa@users.noreply.github.com>
2023-10-22 21:47:18 +0700

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+class Config
+{
+	const TITLE = 'Quản lý sinh viên - Đại học công nghiệp Hà nội';
+	const COPYRIGHT = "Copyright © 2023 By Nhóm 5 Đồ án chuyên ngành";
+	const VERSION = "3.3.0";
+	const OWNER = "Nhóm 5 Đồ án chuyên ngành ";
+	const CONTRIBUTOR = "";
+	const EMAIL = "hungbn619@gmail.com";
+	const RELEASE = "16/10/2023";
+	const TIMEZONE = "Asia/Bangkok";
+}
